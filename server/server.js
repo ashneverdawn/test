ee3ebfc7ba78ad1fd0f0db676845ec508e6e7d4d
Hugo Lindsay <ashneverdawn@gmail.com>
2019-09-29 22:22:57 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,20 +7,12 @@ ES.V1.EventTrigger("event", 1, 2, 3)
 function test(a, b, c) {
   
   var characters = ES.V1.GetPlayerCharacters() 
-  if (characters == null) {
-    console.log("Character list null")
-    return
-  }
-  
-  console.log(characters.length)
   if(characters.length == 0) {
-     ES.V1.AddPlayerCharacter('TestName')
+     ES.V1.AddPlayerCharacter(ES.V1.GetUsername())
      console.log('creating...')
   }
   
-  for (var i = 0; i < characters.length; i++) {
-    console.log(characters[i].Name)
-  }
-  console.log(characters)
+  console.log('selecting...')
+  ES.V1.SelectPlayerCharacter(ES.V1.GetUsername())
   
 }
