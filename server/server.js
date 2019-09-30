ef98082684bd4e08cd6c03c91d978d3bea94eaff
Hugo Lindsay <ashneverdawn@gmail.com>
2019-09-29 20:04:08 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 //Server test
 
-ES.V1.Apitest()
-
 ES.V1.EventListen("event", "test")
 ES.V1.EventTrigger("event", 1, 2, 3)
 
-var abc = 2 + 2;
-console.log("server/server.js: The value of abc is " + abc); // 4
 
 function test(a, b, c) {
-  abc += a
-  console.log("abc += a: " + abc)
-  abc += b
-  console.log("abc += b: " + abc)
-  abc += c
-  console.log("abc += c: " + abc)
+  
+  var characters = ES.V1.GetUserCharacters() 
+  if (characters == null) {
+    console.log("Character list null")
+    return
+  }
+  
+  console.log(characters.length)
+  console.log(characters)
+  
 }
